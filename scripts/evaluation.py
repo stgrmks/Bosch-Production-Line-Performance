cb43d77d940248d1aca2d891559849a4a002ad13
m.steger@protonmail.com <neuburg5DFGH!>
2018-04-08 17:11:32 +0200

    clean up

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -4,9 +4,8 @@ import numpy as np
 import pandas as pd
 import os
 from sklearn import metrics, model_selection, ensemble
-from functools import partial
 
-def mcc_treshold(Y, Yhat, grid=10):
+def mcc_treshold(Y, Yhat, grid = 10):
     Yhat = Yhat[:, 1]
     thresholds = np.linspace(0.99, 0.99999, grid)
     mcc = np.array([metrics.matthews_corrcoef(Y, (Yhat > np.percentile(Yhat, t * 100)).astype(int)) for t in thresholds])
@@ -16,14 +15,12 @@ if __name__ == '__main__':
     data_folder = r'/home/mks/ownCloud/machine learning/P3/'
     seed = 1337
     folds = 2
-    mcc_grid = 10
 
     model = ensemble.RandomForestClassifier()
     X_train, y_train = pd.read_csv(os.path.join(data_folder, 'features_train.csv.gz'), index_col = 0), pd.read_csv(os.path.join(data_folder, 'Y.csv.gz'), usecols = ['Response'])['Response'].astype(np.int8)
     skf = model_selection.StratifiedKFold(n_splits = folds, shuffle = True, random_state = seed)
-    scorer = metrics.make_scorer(score_func = partial(func = mcc_treshold, grid = mcc_grid), greater_is_better = True, needs_proba = True)
+    scorer = metrics.make_scorer(score_func = mcc_treshold, greater_is_better = True, needs_proba = True)
 
     result = model_selection.cross_val_score(estimator = model, X = X_train, y = y_train, scoring = scorer, cv = skf, n_jobs = 1)
 
     print 'mean: {} std: {} performance: {}'.format(np.mean(result), np.std(result), result)
-    print 'done'
